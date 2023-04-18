0cba8c5e87aff4db4673fc3c74f75e719285d6be
gogiari <gomul82@gmail.com>
2023-04-18 17:41:52 +0900

    first commit

---
+++ src/main/java/com/example/com/user/conroller/UserController.java
@@ -0,0 +1,15 @@
+package com.example.com.user.conroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/User")
+public class UserController {
+    
+    @RequestMapping("/List")
+    public String list() {
+        return "list";
+    }
+    
+}
