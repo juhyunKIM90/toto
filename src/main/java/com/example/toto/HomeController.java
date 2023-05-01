43600af76eb216a7fc45db5e5c7136416d4c839c
lck9677 <lck9677@icloud.com>
2023-05-01 11:23:49 +0900

    이창경 home

--- src/main/java/com/example/toto/HomeController.java
+++ src/main/java/com/example/toto/HomeController.java
@@ -9,8 +9,17 @@ public class HomeController {
     public String home(){
         // return "home";
         //return "develmap"; // 작업 편하라고 바로가기 느낌? 사이트경로(후에 사이트맵으로 사용해도되고)
-        return "homekyung";
+        return "home_start";
     }
     
-   
+   @RequestMapping("/homekyung")
+   public String homekyung(){
+
+    return "homekyung";
+   }
+   @RequestMapping("/loginpage")
+   public String loginpage(){
+
+    return "loginpage";
+   }
 }
--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,3 +1,49 @@
+
+// -----------------------------------------------------------------------------------------start home js
+let target = document.querySelector("#dynamic");
+let stringArr = ["Let's Soccer. . . . .","Let's basketball . . . . .","Let's baseball . . . . . ",];
+
+let selectString = stringArr[Math.floor(Math.random()*stringArr.length)];
+let selectStringArr = selectString.split("");
+
+
+function randomString(){
+    
+    let target = document.querySelector("#dynamic");
+    let stringArr = ["Let's Soccer . . . . . ","Let's basketball . . . . . ","Let's baseball . . . . . ",];
+    let selectString = stringArr[Math.floor(Math.random()*stringArr.length)];
+    let selectStringArr = selectString.split("");
+    
+    return selectStringArr;
+}
+
+//타이핑 리셋
+function resetTyping(){
+  target.textContent = "";
+  dynamic (randomString());
+}
+
+
+//한글자씩 텍스트 출력 함수
+function dynamic(randomArr){
+    if(randomArr.length > 0){
+        target.textContent += randomArr.shift();
+        setTimeout(function(){
+            dynamic(randomArr);
+        },100);
+    }else{
+        setTimeout(resetTyping, 3000);
+    }
+}
+
+dynamic (randomString());
+
+function blink(){
+    
+    target.classList.toggle("active");
+    
+}
+// --------------------------------------------------------------------------------- home js
 const logregBox  = document.querySelector('.logreg-box');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
@@ -8,3 +54,4 @@ registerLink.addEventListener('click', () => {
 loginLink.addEventListener('click', () => {
     logregBox.classList.remove('active');
 });
+setInterval(blink, 500);
