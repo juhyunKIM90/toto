e0857ca1b50d35f3b5a9d0a8f6b77ccc0311bfeb
gogiari <gomul82@gmail.com>
2023-05-15 14:56:31 +0900

    게시판 분류 일단 완료

--- src/main/java/com/example/toto/board/BoardController.java
+++ src/main/java/com/example/toto/board/BoardController.java
@@ -9,23 +9,26 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.example.toto.board.service.BoardService;
 import com.example.toto.board.vo.BoardVo;
+import com.example.toto.menu.service.MenuService;
+import com.example.toto.menu.vo.MenuVo;
 
 @Controller
 //@RequestMapping("/Board")
 public class BoardController {
     @Autowired
     private BoardService boardService;
-    //private MenuService  menuService;
+    @Autowired
+    private MenuService  menuService;
     
     @RequestMapping("/Board/List")
     public ModelAndView list(String menu_id) {
         ModelAndView mv = new ModelAndView();
 
-        // 메뉴
-        //List<MenuVo> menuList = menuService.getMenuList();
-        //System.out.println(menuList);
-
         System.out.println("키미노메뉴아디와? " + menu_id);
+
+        // 메뉴
+        MenuVo menu = menuService.getMenu(menu_id);
+
 
 
         // BoardList - 게시글 목록
@@ -33,7 +36,7 @@ public class BoardController {
 
         mv.setViewName("board/list");
         mv.addObject("boardList", boardList);
-        //mv.addObject("menuList", menuList);
+        mv.addObject("menu", menu);
         //System.out.println("nope" + menuList);
         return mv;
     }
@@ -48,7 +51,7 @@ public class BoardController {
     
     @RequestMapping("/Board/Write")
     public ModelAndView write(BoardVo boardVo) {
-        ModelAndView mv = new ModelAndView("redirect:/Board/List");
+        ModelAndView mv = new ModelAndView("redirect:/Board/List?menu_id=" + boardVo.getMenu_id() );
         
         // 게시글 등록
         boardService.insertBoard(boardVo);
@@ -71,24 +74,24 @@ public class BoardController {
     public ModelAndView updateForm (String idx) {
         ModelAndView mv = new ModelAndView("board/update");
         BoardVo boardVo = boardService.getBoard( idx );
+        System.out.println("가져왓니??" + boardVo);
         mv.addObject("boardVo", boardVo);
         return mv;
     }
 
     @RequestMapping("/Board/Update")
     public ModelAndView update(BoardVo boardVo) {
-        System.out.println("가져왓니??" + boardVo);
 
-        ModelAndView mv = new ModelAndView("redirect:/Board/View?idx=" + boardVo.getIdx());
+        ModelAndView mv = new ModelAndView("redirect:/Board/View?idx=" + boardVo.getIdx() + "&menu_id=" + boardVo.getMenu_id() );
         boardService.updateBoard(boardVo);
         return mv;
     }
 
     // 게시글 삭제 ---------------------------------------------------------------------------
     @RequestMapping("/Board/Delete")
-    public ModelAndView delete(String idx) {
+    public ModelAndView delete(String idx, String menu_id) {
         System.out.println("idx는 뭐고? " + idx);
-        ModelAndView mv = new ModelAndView("redirect:/Board/List");
+        ModelAndView mv = new ModelAndView("redirect:/Board/List?menu_id=" + menu_id);
         boardService.deleteBoard(idx);
         return mv;
     }
