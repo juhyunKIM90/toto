e717e98de58cbe8c323a3cab5512d05c57646641
jihooony <chic8962@gmail.com>
2023-05-15 11:41:34 +0900

    로그인 시쿠리티

---
+++ src/main/java/com/example/toto/loginSecurity/SecurityConfiguration.java
@@ -0,0 +1,47 @@
+package com.example.toto.loginSecurity;
+ 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+ 
+@EnableWebSecurity
+public class SecurityConfiguration {
+ 
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		/* @formatter:off */
+		http
+			.authorizeRequests()
+				.antMatchers("/css/**","/", "/home","/js/**","/img/**","/templates/**").permitAll() // 설정한 리소스의 접근을 인증절차 없이 허용
+				.anyRequest().authenticated() // 그 외 모든 리소스를 의미하며 인증 필요
+				.and()
+			.formLogin()
+				.loginPage("/loginpage") // 기본 로그인 페이지
+				.permitAll()
+				.and()
+			.logout()
+				.permitAll();
+		
+		return http.build();
+		/* @formatter:on */
+	}
+ 
+	@Bean
+	public UserDetailsService userDetailsService() {
+		/* @formatter:off */
+		UserDetails user =
+			 User.withDefaultPasswordEncoder()
+				.username("user")
+				.password("pass")
+				.roles("USER")
+				.build();
+		/* @formatter:on */
+ 
+		return new InMemoryUserDetailsManager(user); // 메모리에 사용자 정보를 담는다.
+	}
+}
